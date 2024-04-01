36d32ee41ad0cab44feaf789eb94fc161a4edee5
Krishna Mandal <43015249+KrishnaM251@users.noreply.github.com>
2024-04-01 11:48:52 -0700

    Update hello_world.rs

--- hw8-cosmwasm/ex1/hello_world.rs
+++ hw8-cosmwasm/ex1/hello_world.rs
@@ -1,6 +1,6 @@
 fn main() {
     let hello_msg = "Hello Universe!";
-    let species_msg = "I'm an Alien";
+    let species_msg = "I'm a Rustacean";
 
     // Add your code to print `hello_msg` here
     // Add your code to print `species_msg` here
